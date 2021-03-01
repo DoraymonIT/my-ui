0eb9ab28c79e95892f7776a239643eae19f3afe6
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-03-01 22:54:22 +0100

    notes + alerts + 50% navBars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { MyCardComponent } from './my-card/my-card.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 import { MyInputComponent } from './my-input/my-input.component';
+import { MyNotesAlertsComponent } from './my-notes-alerts/my-notes-alerts.component';
+import { MyNavBarComponent } from './my-nav-bar/my-nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { MyInputComponent } from './my-input/my-input.component';
     MyCardComponent,
     MyButtonComponent,
     MyInputComponent,
+    MyNotesAlertsComponent,
+    MyNavBarComponent,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/my-notes-alerts/my-notes-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-notes-alerts',
+  templateUrl: './my-notes-alerts.component.html',
+  styleUrls: ['./my-notes-alerts.component.css']
+})
+export class MyNotesAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
